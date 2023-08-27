486a85891647294139ed30484468da682b76074e
Ket0o <kupregion3@gmail.com>
2023-08-27 19:04:24 +0700

    1. Подправил работу приложения (ViewModel) 2. Добавил свойство, блокирующее поля и кнопку 3. Добавил валидацию 4. Закомментировал Model

--- Model/Classes/FourthDegree.cs
+++ Model/Classes/FourthDegree.cs
@@ -12,9 +12,14 @@ namespace Model.Classes
     /// </summary>
     public class FourthDegree : Function
     {
-
+        /// <summary>
+        /// Возвращает и задает список коэффициента C для функции 4-ой степени.
+        /// </summary>
         public override int[] TypeCoefficientC => new int[5] { 1000, 2000, 3000, 4000, 5000 };
 
+        /// <summary>
+        /// Создает экземпляр класса <see cref="FourthDegree"/>.
+        /// </summary>
         public FourthDegree() 
         {
             NameFunction = Functions.FourthDegree.ToString();
